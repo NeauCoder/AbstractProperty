4be561dc0ba41783d401dae79b231bd3c6fb71a3
codelover <386273319@qq.com>
2019-04-03 12:21:54 +0800

    添加项目文件。

---
+++ AbstractProperty/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class AbstractProperty
+{
+    class Program
+    {
+        public abstract class Person
+        {
+            public abstract string Name { get; set; }
+            public abstract int Age { get; set; }
+        }
+        public class Student : Person
+        {
+            public string Code { get; set; } = "N.A";
+            public override string Name { get; set; } = "N.A";
+            public override int Age { get; set; } = 0;
+            public override string ToString()
+            {
+                return $"Code:{Code},Name:{Name},Age:{Age}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            var s = new Student()
+            {
+                Code = "001",
+                Name = "Zara",
+                Age = 10
+            };
+            Console.WriteLine($"Student Info:={s}");
+            s.Age++;
+            Console.WriteLine($"Student Info:={s}");
+        }
+    }
+}
